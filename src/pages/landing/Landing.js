e6058dec38fa8c4c6eae346c14ed7baaf1328378
Victoria Le <leqvictoria@gmail.com>
2023-03-24 16:55:00 -0400

    landing content

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -5,7 +5,12 @@ import landingBg from "../../assets/videos/landingBG.mp4"
 const Landing = () => {
   return (
     <div className='main'>
-      <video src={landingBg} autoPlay muted />
+      <video src={landingBg} autoPlay muted loop/>
+      <div className='content'>
+        <h1>Welcome to Viet Nam</h1>
+        <p>The only guide you'll need when planning to next trip to Viet Nam.</p>
+        <span>Less Planning, More Enjoying</span>
+      </div>
     </div>
   )
 }
