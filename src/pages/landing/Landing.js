1efac172d3d967166d105a03e6cba2d95fb190a8
Victoria Le <leqvictoria@gmail.com>
2023-03-26 23:54:17 -0400

    landing page styling

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -8,7 +8,7 @@ const Landing = () => {
       <video src={landingBg} autoPlay muted loop/>
       <div className='content'>
         <h1>Welcome to Viet Nam</h1>
-        <p>The only guide you'll need when planning to next trip to Viet Nam.</p>
+        <p>The only guide you'll need when planning your next trip to Viet Nam.</p>
         <span>Less Planning, More Enjoying</span>
       </div>
     </div>
