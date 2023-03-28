fbe0da3e5e012420c6210e7b2d41edae25ba9002
Victoria Le <leqvictoria@gmail.com>
2023-03-28 13:21:02 -0400

    landing page with custom font

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -4,12 +4,20 @@ import landingBg from "../../assets/videos/landingBG.mp4"
 
 const Landing = () => {
   return (
-    <div className='main'>
-      <video src={landingBg} autoPlay muted loop/>
-      <div className='content'>
-        <h1>Welcome to Viet Nam</h1>
-        <p>The only guide you'll need when planning your next trip to Viet Nam.</p>
-        <span>Less Planning, More Traveling.</span>
+    <div className='landingPage'>
+      <video className='video-bg' src={landingBg} autoPlay muted loop />
+      <div className='bg-overlay'></div>
+      <div className='navBar'>
+        <div className='menu'>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+
+      <div className='home-text'>
+        <h1>Welcome to Viet Guy</h1>
+        <p>Let's plan your next trip to Viet Nam with us</p>
       </div>
     </div>
   )
