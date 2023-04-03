205c8ff5acc51bedfd604cc6c86395ca9ddb2208
Victoria Le <leqvictoria@gmail.com>
2023-04-03 15:50:25 -0400

    top picks

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -13,6 +13,7 @@ const Home = () => {
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
+        <h1 className="homeTitle">Our Top Picks</h1>
       </div>
     </div>
   )
