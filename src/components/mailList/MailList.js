f5fb37b1d81eaeb3933b5936ab81e334fb24a0d4
Victoria Le <leqvictoria@gmail.com>
2023-04-04 21:45:46 -0400

    mailing comp

---
+++ src/components/mailList/MailList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './mailList.css'
+
+const MailList = () => {
+  return (
+    <div>
+      This is the mailing list 
+    </div>
+  )
+}
+
+export default MailList
