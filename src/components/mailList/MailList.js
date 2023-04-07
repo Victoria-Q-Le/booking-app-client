ce57e21db31f565f3c4b4d96ce6c4928cf263de8
Victoria Le <leqvictoria@gmail.com>
2023-04-07 18:59:34 -0400

    mailing comp styling

--- src/components/mailList/MailList.js
+++ src/components/mailList/MailList.js
@@ -3,9 +3,14 @@ import './mailList.css'
 
 const MailList = () => {
   return (
-    <div>
-      <input></input>
-    </div>
+    <div className='mail'>
+      <h1 className='mailTitle'>Save time, save money!</h1>
+      <span className='mailDesc'>Subcribe to receive best deal everyday</span>
+      <div className='mailInputContainer'>
+        <input type='text' placeholder='Enter Your Email'></input>
+        <button>Subcribe</button>
+      </div>
+    </div> 
   )
 }
 
