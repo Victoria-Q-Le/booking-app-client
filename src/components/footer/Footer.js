54e7b8217e34b1a34fed1a1bf77ae4a483beb3f6
Victoria Le <leqvictoria@gmail.com>
2023-04-10 22:41:06 -0400

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,8 +2,29 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      This is the footer
+    <div className='footer'>
+      <div className='fLists'>
+        <ul className='fList'>
+          <li className='fListItem'>Cities</li>
+          <li className='fListItem'>Districts</li>
+          <li className='fListItem'>Airports</li>
+          <li className='fListItem'>Hotels</li>
+          <li className='fListItem'>Place of interest</li>
+        </ul>
+
+        <ul className='fList'>
+          <li className='fListItem'>Car rental</li>
+          <li className='fListItem'>Flight finder</li>
+          <li className='fListItem'>Restaurant reservations</li>
+        </ul>
+
+        <ul className='fList'>
+          <li className='fListItem'>Customer service help</li>
+          <li className='fListItem'>Careers</li>
+          <li className='fListItem'>Privacy and cookie statement</li>
+          <li className='fListItem'>Terms & Conditions</li>
+        </ul>
+      </div>
     </div>
   )
 }
