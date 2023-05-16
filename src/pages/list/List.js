b6c4f1b42341ddb08d83fee22ef7622f784235ab
Victoria Le <leqvictoria@gmail.com>
2023-05-16 11:30:32 -0400

    list search styling

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -9,7 +9,9 @@ const List = () => {
         <Header type="list"/>
         <div className="listContainer">
           <div className="listWrapper">
-            <div className="listSearch"></div>
+            <div className="listSearch">
+              <h1 className="lsTitle">Search</h1>
+            </div>
             <div className="listResult"></div>
           </div>
         </div>
